5bb394d08a60dacd6147714921058dd2929722e1
Dmytro Osypiuk <dima.osypiuk.do@epam.com>
2023-03-03 17:48:08 +0100

    build(dynamodb): updated links for env, app is working

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://0nasdwjlua.execute-api.us-east-1.amazonaws.com/dev',
+    bff: 'http://localhost:3000/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
