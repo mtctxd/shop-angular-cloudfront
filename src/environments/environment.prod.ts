729cc02b1a5a42261692f637e7844ac34788f5c0
Dmytro Osypiuk <dima.osypiuk.do@epam.com>
2023-03-11 12:24:23 +0100

    build(s3): updated endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://2narrlsywd.execute-api.us-east-1.amazonaws.com/dev',
     bff: 'https://0nasdwjlua.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
